1b8e839686ea30ce67db54b3119439f206bbb75c
Hayrunnisa Özdemir <hayrunnisaozdemir6141@gmail.com>
2025-05-19 20:17:48 +0300

    Admin paneli sayfa yönlendirme linkleri eklendi.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html lang="tr">
 <head>
-    <title>Seller Dashboard</title>
+    <meta charset="UTF-8">
+    <title>Satıcı Paneli</title>
     <link rel="stylesheet" href="{{ asset('css/admindashboard.css') }}">
 </head>
 <body>
-    <h1>Seller Paneline Hoş Geldiniz</h1>
 
-    <p><strong>Adınız:</strong> {{ Auth::user()->name }}</p>
-    <p><strong>Email:</strong> {{ Auth::user()->email }}</p>
-    <p><strong>Rol:</strong> {{ Auth::user()->role }}</p>
+    <nav class="navbar">
+        <div class="navbar-left">
+            <img src="{{ asset('images/seller-icon.png') }}" alt="Satıcı İkonu" class="seller-icon">
+            <span>Hoş geldiniz, Satıcı XX</span>
+        </div>
 
-    <a href="{{ route('logout') }}">Çıkış Yap</a>
+        <div class="navbar-right">
+            <a href="satis-kitaplar.html">Satıştaki Kitaplar</a>
+            <a href="satilan-kitaplar.html">Satılan Kitaplar</a>
+
+    <a href="kazanc.html">
+        <img src="{{ asset('images/coins.png') }}" alt="Kazanç İkonu" class="nav-icon">
+        Kazanç
+    </a>
+
+
+    <a href="cikis.html">
+        <img src="{{ asset('images/user-logout.png') }}" alt="Çıkış İkonu" class="nav-icon">
+        Çıkış Yap
+    </a>
+        </div>
+    </nav>
+
+    <div class="content">
+
+    </div>
+
 </body>
 </html>
